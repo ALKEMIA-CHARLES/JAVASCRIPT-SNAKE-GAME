c398dd48d4daeb681f907faa318d73b39c1e80a8
ALKEMIA-CHARLES <charlesmtawalijr@gmail.com>
2024-04-30 10:10:43 +0300

    finish

--- game.js
+++ game.js
@@ -1,10 +1,21 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
+
 function main(currentTime) {
+    if (gameOver){
+        if (confirm('You lost, Press ok to restart')){
+            window.location = '/'
+        }
+        return
+    }
+
+
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -23,6 +34,7 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake()
     updateFood()
+    checkDate()
 }
 
 
@@ -30,4 +42,8 @@ function draw() {
     gameBoard.innerHTML = ''
     drawSnake(gameBoard)
     drawFood(gameBoard)
+}
+
+function checkDate() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
